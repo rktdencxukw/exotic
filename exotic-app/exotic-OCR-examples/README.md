583351e7ac5edc4893ffe4c9acb8e0c7add61daf
kc <kc@mac>
2023-03-23 13:34:41 +0800

    delete exotic-app examples

--- exotic-app/exotic-OCR-examples/README.md
+++
@@ -1,4 +0,0 @@
-# Run executable jar
-
-    java -jar exotic-OCR-examples*.jar
-    java -jar exotic-OCR-examples*.jar -pc 8 -tab 10 -supervised -site walmart
--- exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/OpenChrome.kt
+++
@@ -1,9 +0,0 @@
-package ai.platon.exotic.examples
-
-import ai.platon.pulsar.context.PulsarContexts
-
-fun main() {
-    val session = PulsarContexts.createSession()
-    session.load("https://www.tmall.com/", "-refresh")
-    readLine()
-}
